8b3e7de6b90b98f7b217f24256a4ee49ac792c13
Chandrakant Shinde <chandrakant.shinde@myridz.com>
2017-03-14 10:39:53 +0530

    UI changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
+	validates :name, :price, :image, :provider , presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0}
+
 	mount_uploader :image, ImageUploader
 end

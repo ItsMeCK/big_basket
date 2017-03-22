86da5cd813e24de8b45cfd58bedaca2af13ee0c0
Chandrakant Shinde <chandrakant.shinde@myridz.com>
2017-03-22 14:42:48 +0530

    Multiple images

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
 	validates :price, numericality: {greater_than_or_equal_to: 0}
 
-	mount_uploader :images, ImageUploader
-    mount_uploaders :avatars, ImageUploader
-  	serialize :avatars, JSON # If you use SQLite, add this line.
+  mount_uploaders :avatars, ImageUploader
+  serialize :avatars, JSON # If you use SQLite, add this line.
 end
